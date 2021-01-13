3d94998b71cf1d948b94aa8b73b062d015d276f1
uniqs <uniqs@163.com>
2021-01-13 20:05:12 +0800

    cc

--- main.go
+++ main.go
@@ -1,41 +1,33 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"fmt"
+	"html/template"
 	"net/http"
 )
 
-func sayHello(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "hello golang!",
-	})
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	// 2.解析模板
+	t, err := template.ParseFiles("./hello.tmpl")
+	if err != nil {
+		fmt.Println("template.ParseFiles err:", err)
+		return
+	}
+	// 3.渲染模板
+	name := "哈哈哈哈"
+	err = t.Execute(w, name)
+	if err != nil {
+		fmt.Println("t.Execute err:", err)
+		return
+	}
+
 }
 
 func main() {
-	r := gin.Default()
-
-	r.GET("/hello", sayHello)
-
-	r.GET("/book", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{
-			"method": "GET",
-		})
-	})
-	r.POST("/book", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{
-			"method": "POST",
-		})
-	})
-	r.PUT("/book", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{
-			"method": "PUT",
-		})
-	})
-	r.DELETE("/book", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{
-			"method": "DELETE",
-		})
-	})
-
-	r.Run(":9090")
+	http.HandleFunc("/", sayHello)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		fmt.Println("ListenAndServe err:", err)
+		return
+	}
 }
